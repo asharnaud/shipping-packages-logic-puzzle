e1b8337c635e52ca48626d696a8bf9c5fa629d34
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-03-12 23:00:51 -0500

    Added loops for all possible combinations.

--- puzzle.js
+++ puzzle.js
@@ -1,9 +1,10 @@
 // Step#1 Data Types:
-var firstNameArr = ['Ellen', 'Heather', 'Rick', 'Walter']
-var lastNameArr = ['Bartley', 'DeForest', 'Fairview', 'Gray']
-var statesArr = ['Ohio', 'Montana', 'Texas', 'Washington']
-var eventsArr = ['anniversary', 'birthday', 'house warming', 'wedding']
-var relationshipArr = ['cousin', 'father', 'friend', 'sister']
+var firstName = ['Ellen', 'Heather', 'Rick', 'Walter']
+var lastName = ['Bartley', 'DeForest', 'Fairview', 'Gray']
+var states = ['Ohio', 'Montana', 'Texas', 'Washington']
+var partyType = ['anniversary', 'birthday', 'house warming', 'wedding']
+var relationship = ['cousin', 'father', 'friend', 'sister']
+var day = ['Wednesday', 'Thursday', 'Friday', 'Saturday']
 // Step#2 Logical Statements:
 // Greg’s friend wasn’t Ellen Fairview, who didn’t live in Ohio. The birthday girl didn’t have her party on Friday.
 if (relationship !== 'friend' && state !== 'Ohio') {
@@ -37,3 +38,17 @@ if (lastname !== 'DeForest' && event === 'anniversary' && state === 'Montana') {
 }
 
 //Step#3
+  var combos = []
+  for (var i = 0; i < firstName.length; i++) {
+     for(var j = 0; j < lastName.length; j++) {
+       for(var n = 0; n < states.length; n++) {
+         for(var m = 0; m < partyType.length; m++) {
+           for(var z = 0; z < relationship.length; z++) {
+             for(var k = 0; k < day.length; k++) {
+        combos.push(firstName[i] + lastName[j] + states[n] + partyType[m] + relationship[z] + day[k])
+              }
+            }
+          }
+        }
+     }
+   }
